3fdcb6c944fe6999c6c4943ddf5078abe189e55c
Alasdair <ahurst@appcelerator.com>
2015-11-27 01:39:01 +0000

    remove leftover piece

--- Empire - The Last Battle/Assets/Unity/Scripts/ScriptableObjects/TileData.cs
+++ Empire - The Last Battle/Assets/Unity/Scripts/ScriptableObjects/TileData.cs
@@ -4,6 +4,5 @@ using System.Collections.Generic;
 public class TileData : ScriptableObject {
     public TerrainType Terrain;
     public BuildingType Building;
-    public List<TileData> Adjacent;
     public Vector2 HeightBetween;
 }
