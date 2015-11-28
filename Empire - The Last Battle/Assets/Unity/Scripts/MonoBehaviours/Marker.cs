331d397d1033bc7f4d5330c58263471dd884693a
Alasdair <ahurst@appcelerator.com>
2015-11-28 14:02:37 +0000

    p16 added 3d markers of different types showing rotation and position

---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/Marker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Marker : MonoBehaviour {
+    public MarkerType Type;
+}
+
+public enum MarkerType {
+    Flag,
+    CommanderStormshapers,
+    CommanderBattlebeards,
+    PlayerScouts,
+    PlayerPikemen,
+    PlayerArchers,
+    PlayerAxeThrowers,
+    PlayerWarriors,
+    PlayerCavalry,
+    PlayerCatapult,
+    PlayerBallista,
+    PlayerSingleUnit,
+    ActiveOpposition,
+    ReserveOppositionA,
+    ReserveOppositionB
+}
