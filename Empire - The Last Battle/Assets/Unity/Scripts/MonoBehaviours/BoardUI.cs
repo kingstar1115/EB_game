36aaf8bd0ae01993381de8c107cad42b71f53cc7
Steven P-S <stevenpuertosimmons@gmail.com>
2015-11-29 17:57:29 +0000

    Started commander movement in new "Steven" test scene. Added tag for tiles. Can drag a box that will snap to tiles of the board.

---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/BoardUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardUI : MonoBehaviour 
+{
+
+    public static bool GetTileHovered_Position(out Collider tileCollider)
+    {
+        //raycast for a collider tagged tile and return position
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Tile")
+            {
+                Debug.Log("TileHovered");
+                tileCollider = hit.collider;
+                return true;
+            }
+        }
+
+        tileCollider = null;
+        return false;
+    }
+}
---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/CommanderUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommanderUI : MonoBehaviour
+{
+
+    Collider collider;
+
+	// Use this for initialization
+	void Start () 
+    {
+        collider = this.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDrag()
+    {
+        //float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        //transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+
+        //if hovered a tile then move player to that place
+        Collider hoveredCollider;
+        if (BoardUI.GetTileHovered_Position(out hoveredCollider))
+        {
+            Debug.Log("Dragging over a tile");
+            this.transform.position = new Vector3(hoveredCollider.transform.position.x, 
+                hoveredCollider.bounds.max.y + collider.bounds.extents.y, 
+                hoveredCollider.transform.position.z);
+        }
+    }
+}
