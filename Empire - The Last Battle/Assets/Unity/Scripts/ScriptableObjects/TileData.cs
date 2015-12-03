487db7feb57923f0ee0873631cc1004ba680effa
unknown <u1272692@CW504-05.AD.HUD.AC.UK>
2015-12-03 13:10:24 +0000

    p20-tile-refactor-wip

--- Empire - The Last Battle/Assets/Unity/Scripts/ScriptableObjects/TileData.cs
+++ Empire - The Last Battle/Assets/Unity/Scripts/ScriptableObjects/TileData.cs
@@ -1,8 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+[System.Serializable]
 public class TileData : ScriptableObject {
     public TerrainType Terrain;
     public BuildingType Building;
-    public float Height;
+
+	[System.NonSerialized]
+	public List<TileData> ConnectedTiles;
+	public int X;
+	public int Y;
+
+
+	public PlayerType Owner = PlayerType.Neutral;
+	public float Height;
+
+	public void AddConnectedTile(TileData t) {
+		if (ConnectedTiles == null) {
+			ConnectedTiles = new List<TileData>();
+			ConnectedTiles.Add(t);
+		}
+		else if (!ConnectedTiles.Contains (t)) {
+			ConnectedTiles.Add(t);
+		}
+	}
+
+	public List<TileData> GetConnectedTiles() {
+		if (ConnectedTiles == null) {
+			return new List<TileData>();
+		}
+		return ConnectedTiles;
+	}
 }
