15a0963bfc34ad38a65389bce5b60e1d6a8dec7c
Steven P-S <stevenpuertosimmons@gmail.com>
2015-12-15 11:03:36 +0000

    P45 - Duplicated Alasdair scene to update Steven scene references. Added lost immortal player kill count and edited armory to return available castle pieces.

--- Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/Player.cs
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/Player.cs
@@ -7,12 +7,29 @@ public class Player : MonoBehaviour
 	public PointsSystem Currency;
 	public CardData Hand;
 	public Army PlayerArmy;
-
 	public int CastleProgress;
 
-    public void SetPosition()
+    int lostImortalKillCount;
+    public int LostImortalKillCount
     {
-    
+        get
+        {
+            return lostImortalKillCount;
+        }
+    }
+
+    public void Reset()
+    {
+        lostImortalKillCount = 0;
+    }
+
+    public void LostImortalKilled_Increment()
+    {
+        //limit the lost imortal kill count
+        if (lostImortalKillCount + 1 <= 4)
+            lostImortalKillCount++;
+        else
+            Debug.LogError("Trying to kill too many lost imiortals");
     }
 }
 public enum PlayerType
