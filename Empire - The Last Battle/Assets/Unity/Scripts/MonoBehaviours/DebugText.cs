e9710e934198799e397991aa44535e000ad218b4
Martin <martino1995@blueyonder.co.uk>
2016-01-20 19:08:04 +0000

    p56-debugui Completed text debug ui ticket

---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/DebugText.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour
+{
+	private DebugUI _debugUI;
+	private Text _text;
+	private Color _baseColor;
+	private float _time;
+
+	void Start()
+	{
+		_debugUI = GameObject.Find("DebugUI").GetComponent<DebugUI>();
+		_text = gameObject.GetComponent<Text>();
+		_baseColor = _text.color;
+	}
+
+	void Update()
+	{
+		_time += 0.01f;
+
+		//If text has some text then start fading
+		if (_text.text.Length > 0)
+			Fade();
+	}
+
+	void Reset()
+	{
+		_text.text = "";
+		_text.material.color = _baseColor;
+		_time = 0f;
+	}
+
+	void Fade()
+	{
+		//If text has fully faded out then reset text variables to default 
+		if (_time > _debugUI._duration)
+		{
+			Reset();
+			return;
+		}
+
+		Color myColor = _text.material.color;
+		float ratio = _time / _debugUI._duration;
+		myColor.a = Mathf.Lerp(1, 0, ratio);
+		_text.material.color = myColor;
+	}
+}
---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/DebugUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DebugUI : MonoBehaviour
+{
+	public float _duration;
+	public List<Text> _text;
+
+	public void SetMessage(string message, int fontSize, Color fontColour)
+	{
+		var firstItem = _text.First();
+		var lastItem = _text.Last();
+
+		foreach (var item in _text)
+		{
+			//If debug text already is showing text then move to next text object
+			if (item.text.Length > 0)
+			{
+				//If on last item, and each text before is full then go ahead and assign variables to first text element
+				if (item.Equals(lastItem))
+				{
+					firstItem.text = message;
+					firstItem.fontSize = fontSize;
+					firstItem.color = fontColour;
+				}
+				continue;
+			}
+
+			item.text = message;
+			item.fontSize = fontSize;
+			item.color = fontColour;
+
+			//variables have been assigned to new text so return from loop
+			//to make sure we don't assign more than 1 text object
+			break;
+		}
+	}
+}
