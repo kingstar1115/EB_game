45182f28b036811353d1bced391f4200914739e4
Martin <u1356770@CW504-03.AD.HUD.AC.UK>
2016-02-04 11:59:14 +0000

    Audio handler added

---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/Audio.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AudioEventArgs : EventArgs
+{
+	//Clip to play
+	public string ClipName { get; set; }
+	///<summary>
+	///0-1
+	///</summary>
+	public float Volume { get; set; }
+	public bool Mute { get; set; }
+	public bool Loop { get; set; }
+	///<summary>
+	///0-256, The higher the number means it takes priority over other sounds, default = 130
+	///</summary>
+	public int Priority 
+	{ 
+		get 
+		{
+			return _priority;
+		} 
+		set 
+		{
+			if (value >= 256)
+				_priority = 256;
+		} 
+	}
+
+	int _priority;
+	
+	public AudioEventArgs()
+	{
+		ClipName = null;
+		Volume = 1.0f;
+		Mute = false;
+		Loop = false;
+		Priority = 130;
+	}
+}
+
+public class Audio : MonoBehaviour
+{
+	public TurnManager TurnManager;
+	public Armoury Armoury;
+	public Army Army;
+	public CardSystem CardSystem;
+	public CommanderUI BattlebeardUI;
+	public CommanderUI StormshaperUI;
+	public OverworldUI OverworldUI;
+
+	private AudioSource _audioSource;
+
+	private static readonly List<AudioClip> Clips = new List<AudioClip>()
+	{
+		//Put audio clips here
+		Resources.Load<AudioClip>("Audio/SFX/cartoon001"),
+
+	};
+
+	// Use this for initialization
+	void Awake()
+	{
+		_audioSource = GetComponent<AudioSource>();
+		AudioHandler ah = new AudioHandler ();
+		ah.Init(this, TurnManager, Armoury, Army, CardSystem, BattlebeardUI, StormshaperUI, OverworldUI);
+	}
+
+	public void Play(object source, AudioEventArgs e)
+	{
+		var clip = Clips.First(x => x.name == e.ClipName);
+		var volume = e.Volume;
+		_audioSource.mute = e.Mute;
+		_audioSource.priority = e.Priority;
+		_audioSource.loop = e.Loop;
+		
+		//PlayOneShot allows multiple sounds to be played on one audio source
+		_audioSource.PlayOneShot(clip, volume);
+	}
+}
---
+++ Empire - The Last Battle/Assets/Unity/Scripts/MonoBehaviours/AudioHandler.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+public class AudioHandler
+{
+	Audio _audio;
+	TurnManager _turnManager;
+	Armoury _armoury;
+	Army _army;
+	CardSystem _cardSystem;
+	CommanderUI _battlebeardUI;
+	CommanderUI _stormshaperUI;
+	OverworldUI _overWorldUI;
+
+	// Use this for initialization
+	public void Init(Audio au, TurnManager tm, Armoury armoury, Army army, CardSystem cs, CommanderUI ssUi, CommanderUI bbUI, OverworldUI oUI)
+	{
+		_audio = au;
+		_turnManager = tm;
+		_armoury = armoury;
+		_army = army;
+		_cardSystem = cs;
+		_battlebeardUI = bbUI;
+		_stormshaperUI = ssUi;
+		_overWorldUI = oUI;
+
+	//	_turnManager.OnSwitchTurn += SwitchTurnListener;
+		//_armoury.OnPurchasedItem += PurchasedItemListener;
+		//_army.OnUpdateUnit += UpdateUnitListener;
+		//_army.OnAddUnit += AddUnitListener;
+		//_army.OnRemoveUnit += RemoveUnitListener;
+		_cardSystem.OnEffectApplied += EffectAppliedListener;
+		_cardSystem.OnCardUseFailed += EffectFailedListener;
+		_battlebeardUI.OnStartDrag += BattlebeardStartDragListener;
+		//_battlebeardUI.OnDropCommander += BattlebeardDropListener;
+		_stormshaperUI.OnStartDrag += StormshaperStartDragListener;
+	//	_stormshaperUI.OnDropCommander += StormshaperDropListener;
+		_overWorldUI.OnPause += PauseListener;
+		_overWorldUI.OnUnPause += UnPauseListener;	}
+
+	void SwitchTurnListener()
+	{
+		_audio.Play(_turnManager, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void PurchasedItemListener(PurchasableItem purchasedItem)
+	{
+		_audio.Play(_armoury, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void UpdateUnitListener(Unit u)
+	{
+		_audio.Play(_army, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void AddUnitListener(Unit u)
+	{
+		_audio.Play(_army, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void RemoveUnitListener(Unit u, int unitIndex)
+	{
+		//Maybe want to play a different sound depending on unit index?
+		//switch (unitIndex) {
+		//default:
+		//	break;
+		//}
+
+		_audio.Play(_army, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void EffectAppliedListener(CardData cd, Player p)
+	{
+		_audio.Play(_cardSystem, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void EffectFailedListener(CardData cd, Player p)
+	{
+		_audio.Play(_cardSystem, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void BattlebeardStartDragListener()
+	{
+		_audio.Play(_battlebeardUI, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void BattlebeardDropListener(TileData td)
+	{
+		_audio.Play(_battlebeardUI, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void StormshaperStartDragListener()
+	{
+		_audio.Play(_stormshaperUI, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void StormshaperDropListener(TileData td)
+	{
+		_audio.Play(_stormshaperUI, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+	
+	void PauseListener()
+	{
+		_audio.Play(_overWorldUI, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+
+	void UnPauseListener()
+	{
+		_audio.Play(_overWorldUI, new AudioEventArgs { ClipName = "cartoon001", Priority = 256 });
+	}
+}
