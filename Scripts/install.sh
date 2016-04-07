7fcd5219c51518b6826fd195565cd3c3092b8913
Ewan Harris <ewanharris93@gmail.com>
2016-04-07 13:59:54 +0100

    Move to 5.3.2 unity pkg as it's smaller, this means windows and mac builds are not possible

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-echo 'Downloading from http://download.unity3d.com/download_unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://download.unity3d.com/download_unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+echo 'Downloading from http://download.unity3d.com/download_unity/e87ab445ead0/MacEditorInstaller/Unity-5.3.2f1.pkg: '
+curl -o Unity.pkg http://download.unity3d.com/download_unity/e87ab445ead0/MacEditorInstaller/Unity-5.3.2f1.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
