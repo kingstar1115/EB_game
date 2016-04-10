9411133c6cafeac0042607b90d9f86313faf9ce4
Ewan Harris <ewanharris93@gmail.com>
2016-04-10 22:55:34 +0100

    Move directory that is built to

--- Empire - The Last Battle/Assets/Unity/Scripts/Editor/Build.cs
+++ Empire - The Last Battle/Assets/Unity/Scripts/Editor/Build.cs
@@ -4,7 +4,11 @@ class MyEditorScript
 {
 	static void PerformBuild ()
 	{
-		string[] scenes = { "Assets/Unity/Scenes/Overworld.unity", "Assets/Unity/Scenes/BattleScene.unity", "Assets/Unity/Scenes/StartMenu.unity" };
-		BuildPipeline.BuildPlayer (scenes, "WebPlayerBuild", BuildTarget.WebPlayer, BuildOptions.None);
+		string[] scenes = { 
+			"Assets/Unity/Scenes/Overworld.unity", 
+			"Assets/Unity/Scenes/BattleScene.unity", 
+			"Assets/Unity/Scenes/StartMenu.unity" 
+		};
+		BuildPipeline.BuildPlayer (scenes, "../Build", BuildTarget.WebPlayer, BuildOptions.None);
 	}
 }
--- Scripts/pushGHPages.sh
+++ Scripts/pushGHPages.sh
@@ -1,21 +1,21 @@
 #! /bin/sh
 
-# if [ "$TRAVIS_REPO_SLUG" == "eharris93/ELB" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "eharris93/ELB" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
 echo "Looks good to publish to gh-page"
 
-cd "Empire - The Last Battle/Build"
+cd Build
 
-cp ../../index.html ./index.html
-cp ../../.gitignore ./.gitignore
+cp ../index.html ./index.html
+cp ../.gitignore ./.gitignore
 
-# git init
-# git config --global user.email "ewanharris93@gmail.com"
-# git config --global user.name "travis-ci"
-#
-# git add .
-# git commit -m "Push to github pages"
-# git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}.git" master:gh-pages > /dev/null 2>&1
+git init
+git config --global user.email "ewanharris93@gmail.com"
+git config --global user.name "travis-ci"
+
+git add .
+git commit -m "Push to github pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}.git" master:gh-pages > /dev/null 2>&1
 
 echo "Publish done"
-# fi
+fi
