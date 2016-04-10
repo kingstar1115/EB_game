a4a7180610c3b61279246e843f03269bfed3aec6
Ewan Harris <ewanharris93@gmail.com>
2016-04-10 23:40:38 +0100

    Fix order of scenes

--- Empire - The Last Battle/Assets/Unity/Scripts/Editor/Build.cs
+++ Empire - The Last Battle/Assets/Unity/Scripts/Editor/Build.cs
@@ -5,9 +5,9 @@ class MyEditorScript
 	static void PerformBuild ()
 	{
 		string[] scenes = { 
+			"Assets/Unity/Scenes/StartMenu.unity" ,
 			"Assets/Unity/Scenes/Overworld.unity", 
-			"Assets/Unity/Scenes/BattleScene.unity", 
-			"Assets/Unity/Scenes/StartMenu.unity" 
+			"Assets/Unity/Scenes/BattleScene.unity"
 		};
 		BuildPipeline.BuildPlayer (scenes, "../Build", BuildTarget.WebPlayer, BuildOptions.None);
 	}
